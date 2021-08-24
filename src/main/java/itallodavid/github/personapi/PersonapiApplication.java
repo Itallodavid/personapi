001b67d707ffef8ca6ecb1d696f4e23ef49cf1f6
Itallodavid <itallodavid.fdasilva@gmail.com>
2021-08-23 22:18:42 -0300

    Initial commit

---
+++ src/main/java/itallodavid/github/personapi/PersonapiApplication.java
@@ -0,0 +1,13 @@
+package itallodavid.github.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
